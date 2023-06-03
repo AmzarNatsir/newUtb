d76dfd4ea8b32517a8c682ecbd160f7977246849
amzar <amzarnatsir79@gmail.com>
2023-06-03 08:02:10 +0800

    Update

--- resources/views/layouts/javascript.blade.php
+++ resources/views/layouts/javascript.blade.php
@@ -28,7 +28,7 @@
 <script src="{{asset('assets/AdminLTE/plugins/inputmask/jquery.inputmask.min.js')}}"></script>
 <!-- date-range-picker -->
 <script src="{{asset('assets/AdminLTE/plugins/daterangepicker/daterangepicker.js')}}"></script>
-<script src="{{asset('assets/AdminLTE/plugins/datepicker/bootstrap-datepicker.js')}}"></script>
+<!-- <script src="{{asset('assets/AdminLTE/plugins/datepicker/bootstrap-datepicker.js')}}"></script> -->
 <script src="{{asset('assets/AdminLTE/plugins/number/jquery.number.js')}}"></script>
 <script src="{{asset('assets/AdminLTE/plugins/datepicker/dcalendar.picker.js')}}"></script>
 <!-- ChartJS -->
