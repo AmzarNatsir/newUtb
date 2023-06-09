247c6aab5e237b649b7668d0ecfa67280540ee7c
amzar <amzarnatsir79@gmail.com>
2023-06-09 20:17:24 +0800

    Update Modul Common Via

--- resources/views/common/via/index.blade.php
+++ resources/views/common/via/index.blade.php
@@ -68,7 +68,32 @@
         </div>
     </div>
 </div>
-<script type="text/javascript" src="{{ asset('assets/js/common/via.js') }}"></script>
+<!-- <script type="text/javascript" src="{{ asset('assets/js/common/via.js') }}"></script> -->
+<script>
+    $(function(){
+        let APP_URL_ADD = route('viaAdd');
+        window.setTimeout(function () { $("#success-alert").alert('close'); }, 2000);
+        $("#tbl_tambah").on("click", function()
+        {
+            $("#frm_modal").load(APP_URL_ADD);
+        });
+    });
+    var goEdit = function(el) {
+        $("#frm_modal").load(route('viaEdit', $(el).val()));
+    }
+
+    function konfirmHapus()
+    {
+        var psn = confirm("Delete Data ?");
+        if(psn==true)
+        {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+</script>
 @endsection
 
 
