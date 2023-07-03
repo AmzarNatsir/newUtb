0b3ed771d9ed9390b16775427be61e6d844b25fb
amzar <amzarnatsir79@gmail.com>
2023-07-03 21:02:54 +0800

    Update

--- resources/views/layouts/javascript.blade.php
+++ resources/views/layouts/javascript.blade.php
@@ -52,7 +52,8 @@
             "autoWidth": false,
             "responsive": true,
             "processing": true,
-            "language" : { processing:  '<i class="fa fa-sync fa-spin fa-2x fa-fw"></i><span>  Loading Data...</span>' } ,
+            'retrieve': true,
+            "language" : { 'processing': '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading..n.</span>' } ,
             // "serverSide": true,
         });
         $(".select2").select2();
