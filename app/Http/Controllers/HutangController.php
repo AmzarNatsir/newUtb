2bdd4c7235d3325581d9692aeae68b0cc2f0e4b7
amzar <amzarnatsir79@gmail.com>
2023-09-10 17:20:26 +0800

    Update 3

--- app/Http/Controllers/HutangController.php
+++ app/Http/Controllers/HutangController.php
@@ -35,7 +35,7 @@ class HutangController extends Controller
         $total_invoice = 0;
         $total_hutang = 0;
         $id_supplier = $request->supplier;
-        $result = ReceiveHeadModel::where('supplier_id', $id_supplier)->where('cara_bayar', 2)->get();
+        $result = ReceiveHeadModel::where('supplier_id', $id_supplier)->where('cara_bayar', 2)->whereNull('status_hutang')->get();
         // dd($result);
         $total_terbayar = \DB::table('hutang')
                                 ->join('receive_head', 'receive_head.id', '=', 'hutang.receive_id')
@@ -43,6 +43,12 @@ class HutangController extends Controller
                                 ->whereNull('hutang.deleted_at')
                                 ->selectRaw('sum(hutang.nominal) as t_nominal')
                                 ->pluck('t_nominal')->first();
+
+        $total_terhutang = \DB::table('receive_head')
+                                ->where('receive_head.supplier_id', $id_supplier)
+                                ->whereNull('receive_head.deleted_at')
+                                ->selectRaw('sum(receive_head.total_receive_net) as t_hutang')
+                                ->pluck('t_hutang')->first();
 
         foreach($result as $list)
         {
@@ -100,7 +106,7 @@ class HutangController extends Controller
             ->json([
                 'all_result' => $html,
                 'totalInvoice' => $total_invoice,
-                'totalHutang' => number_format($total_hutang, 0),
+                'totalHutang' => number_format($total_terhutang, 0),
                 'totalTerbayar' => number_format($total_terbayar, 0),
                 'sisaOutstanding' => number_format(($total_hutang - $total_terbayar), 0)
             ])
